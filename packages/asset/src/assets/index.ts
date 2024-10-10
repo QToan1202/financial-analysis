99d6e0f2c99e67a08d73bff90569d04fc041f04d
Toan <92291488+QToan02@users.noreply.github.com>
2024-10-10 15:34:06 +0700

    Create package for assets

---
+++ packages/asset/src/assets/index.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite-plugin-svgr/client" />
+
+export { default as Apple } from './apple.webp'
+export { default as Facebook } from './facebook.webp'
+export { default as Google } from './google.webp'
