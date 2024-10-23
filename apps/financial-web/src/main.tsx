ed78f06bd003713a01f2569afeaa304cecdd3619
Toan <92291488+QToan02@users.noreply.github.com>
2024-10-23 17:13:49 +0700

    Wrap entry point with QueryClientProvider

--- apps/financial-web/src/main.tsx
+++ apps/financial-web/src/main.tsx
@@ -4,11 +4,22 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import App from './App.tsx'
 import { overrideTheme } from './index.ts'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={overrideTheme}>
       <App />
     </ChakraProvider>
-  </StrictMode>
+  </QueryClientProvider>
+  // </StrictMode>
 )
