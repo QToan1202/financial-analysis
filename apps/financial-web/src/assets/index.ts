0039a0144f794d3082a0fcdb1192c44b2f53ad5a
Toan <92291488+QToan02@users.noreply.github.com>
2024-10-28 16:20:00 +0700

    Add navigation icons

--- apps/financial-web/src/assets/index.ts
+++ apps/financial-web/src/assets/index.ts
@@ -3,3 +3,8 @@
 export { default as Apple } from './apple.webp'
 export { default as Facebook } from './facebook.webp'
 export { default as Google } from './google.webp'
+
+export { default as Letter } from './letter.svg?react'
+export { default as Market } from './market.svg?react'
+export { default as Screener } from './screener.svg?react'
+export { default as Watchlist } from './watchlist.svg?react'
