8e2d6393667faf7aaaaeae3ad6f508ebde1ac9c5
Toan <92291488+QToan02@users.noreply.github.com>
2024-11-01 16:59:13 +0700

    Create helper for load env

--- apps/chatbot/utils/__init__.py
+++ apps/chatbot/utils/__init__.py
@@ -1,1 +1,2 @@
+from utils.helpers import (config_env)
 from utils.preprocess import TextProcessor
---
+++ apps/chatbot/utils/helpers.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv, find_dotenv
+
+
+def config_env():
+
+    _ = load_dotenv(find_dotenv())
