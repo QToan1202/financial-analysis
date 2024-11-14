878dea1d62ad2eca9bf9831f0eda2f9f73b8cdd1
Toan <92291488+QToan02@users.noreply.github.com>
2024-11-14 16:05:56 +0700

    Update factory type

--- apps/financial-web/src/factories/index.ts
+++ apps/financial-web/src/factories/index.ts
@@ -3,7 +3,7 @@ export const alphaAdvantagedKeys = {
   lists: () => [...alphaAdvantagedKeys.all, 'list'] as const,
   list: (filters: string) => [...alphaAdvantagedKeys.lists(), { filters }] as const,
   details: () => [...alphaAdvantagedKeys.all, 'detail'] as const,
-  detail: (id: number) => [...alphaAdvantagedKeys.details(), id] as const,
+  detail: (id: string) => [...alphaAdvantagedKeys.details(), id] as const,
 }
 
 export const financialModelingPrepKeys = {
@@ -11,7 +11,7 @@ export const financialModelingPrepKeys = {
   lists: () => [...financialModelingPrepKeys.all, 'list'] as const,
   list: (filters: string) => [...financialModelingPrepKeys.lists(), { filters }] as const,
   details: () => [...financialModelingPrepKeys.all, 'detail'] as const,
-  detail: (id: number) => [...financialModelingPrepKeys.details(), id] as const,
+  detail: (id: string) => [...financialModelingPrepKeys.details(), id] as const,
 }
 
 export const polygonKeys = {
@@ -19,5 +19,5 @@ export const polygonKeys = {
   lists: () => [...polygonKeys.all, 'list'] as const,
   list: (filters: string) => [...polygonKeys.lists(), { filters }] as const,
   details: () => [...polygonKeys.all, 'detail'] as const,
-  detail: (id: number) => [...polygonKeys.details(), id] as const,
+  detail: (id: string) => [...polygonKeys.details(), id] as const,
 }
