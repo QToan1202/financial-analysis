65287cce0e4d7534b73d0ad30bba41160f826ba7
Toan <92291488+QToan02@users.noreply.github.com>
2024-11-20 17:22:40 +0700

    Setup connection for server to Postgres with asyncio

--- apps/chatbot/main.py
+++ apps/chatbot/main.py
@@ -1,30 +1,48 @@
+import sys
+import asyncio
 import os
 from fastapi import FastAPI
 from copilotkit import CopilotKitSDK, LangGraphAgent
 from copilotkit.integrations.fastapi import add_fastapi_endpoint
+from psycopg import AsyncConnection, Connection
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from rag.memory import builder as state_graph
 
-from rag import builder, MongoDBSaver
-
+DB_URI = os.environ.get("PSQL_CONNECTION")
+connection_kwargs = {
+    "autocommit": True,
+    "prepare_threshold": 0,
+}
 app = FastAPI(title="LangGraph Server",
               version="1.0",
               description="A simple api server using CopilotSDK",
               )
 
+if sys.platform:
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-checkpoint = MongoDBSaver.from_conn_info(host=os.environ.get(
-    "MONGODB_ATLAS_CLUSTER_URI"), db_name="langchain_test_db")
-graph = builder.compile(checkpointer=checkpoint)
-config = {"configurable": {"user_id": "1", "thread_id": "chat_thread_1"}}
+sdk: None | CopilotKitSDK = None
 
-sdk = CopilotKitSDK(agents=[
-    LangGraphAgent(
-        name="chat-with-memory-agent",
-        description="Agent that answers interact with human",
-        graph=graph,
-        config=config
-    )
-])
 
+async def setup():
+    global sdk
+    conn = await AsyncConnection.connect(DB_URI, **connection_kwargs)
+    checkpointer = AsyncPostgresSaver(conn)
+    config = {"configurable": {
+        "user_id": "1", "thread_id": "chat_thread_1"}}
+    graph = state_graph.compile(checkpointer=checkpointer)
+
+    sdk = CopilotKitSDK(agents=[
+        LangGraphAgent(
+            name="chat-with-memory-agent",
+            description="Agent that answers interact with human",
+            graph=graph,
+            langgraph_config=config,
+        )
+    ])
+
+
+asyncio.run(setup())
 add_fastapi_endpoint(app, sdk, "copilotkit_remote")
 
 if __name__ == "__main__":
