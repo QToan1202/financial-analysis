2ab5e74b4b473b281ca39f64bc3e6d74031a0312
Toan <92291488+QToan02@users.noreply.github.com>
2024-11-22 17:22:32 +0700

    Update FastAPI event on startup

--- apps/chatbot/main.py
+++ apps/chatbot/main.py
@@ -1,11 +1,11 @@
-import sys
-import asyncio
 import os
 from fastapi import FastAPI
-from copilotkit import CopilotKitSDK, LangGraphAgent
+from copilotkit import CopilotKitSDK, LangGraphAgent, Action as CopilotAction
 from copilotkit.integrations.fastapi import add_fastapi_endpoint
 from psycopg import AsyncConnection, Connection
 from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from langgraph.checkpoint.postgres import PostgresSaver
+from langgraph.checkpoint.memory import MemorySaver
 from rag.memory import builder as state_graph
 
 DB_URI = os.environ.get("PSQL_CONNECTION")
@@ -18,12 +18,8 @@ app = FastAPI(title="LangGraph Server",
               description="A simple api server using CopilotSDK",
               )
 
-if sys.platform:
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-sdk: None | CopilotKitSDK = None
-
-
+@app.on_event("startup")
 async def setup():
     global sdk
     conn = await AsyncConnection.connect(DB_URI, **connection_kwargs)
@@ -32,18 +28,17 @@ async def setup():
         "user_id": "1", "thread_id": "chat_thread_1"}}
     graph = state_graph.compile(checkpointer=checkpointer)
 
-    sdk = CopilotKitSDK(agents=[
-        LangGraphAgent(
-            name="chat-with-memory-agent",
-            description="Agent that answers interact with human",
-            graph=graph,
-            langgraph_config=config,
-        )
-    ])
-
-
-asyncio.run(setup())
-add_fastapi_endpoint(app, sdk, "copilotkit_remote")
+    sdk = CopilotKitSDK(
+        agents=[
+            LangGraphAgent(
+                name="chat-with-memory-agent",
+                description="Agent that answers interact with human",
+                graph=graph,
+                langgraph_config=config,
+            )
+        ],
+    )
+    add_fastapi_endpoint(app, sdk, "copilotkit_remote")
 
 if __name__ == "__main__":
     import uvicorn
