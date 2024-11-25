71fe2024deac3bb0b4a02b43e61bb33e91b6dccc
Toan <92291488+QToan02@users.noreply.github.com>
2024-11-25 14:06:04 +0700

    Move copilotkit runtime to separate file

--- apps/financial-api/src/app.ts
+++ apps/financial-api/src/app.ts
@@ -1,11 +1,6 @@
 import express from 'express'
-import {
-  CopilotRuntime,
-  OpenAIAdapter,
-  copilotRuntimeNodeExpressEndpoint,
-} from '@copilotkit/runtime'
-import OpenAI from 'openai'
-import 'dotenv/config'
+import { connectDB } from './server'
+import { runtime } from './controller/copilotkit'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -18,28 +13,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-const openai = new OpenAI({
-  apiKey: process.env.NVIDIA_API_KEY,
-  baseURL: 'https://integrate.api.nvidia.com/v1',
-})
-const serviceAdapter = new OpenAIAdapter({ openai, model: 'meta/llama-3.1-405b-instruct' })
-
-app.use('/copilotkit', (req, res, next) => {
-  const runtime = new CopilotRuntime({
-    remoteActions: [
-      {
-        url: 'http://localhost:8000/copilotkit_remote',
-      },
-    ],
-  })
-  const handler = copilotRuntimeNodeExpressEndpoint({
-    endpoint: '/copilotkit',
-    runtime,
-    serviceAdapter,
-  })
-
-  return handler(req as any, res, next)
-})
+app.use('/copilotkit', runtime)
 
 app.listen(port, () => {
   return console.log(`http://localhost:${port}`)
