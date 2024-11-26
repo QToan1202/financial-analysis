6a2fba7e8ec7e741905914b5585756e686ef31d8
Toan <92291488+QToan02@users.noreply.github.com>
2024-11-26 16:56:48 +0700

    Update styles in Detail stock page

--- apps/financial-web/src/pages/Detail/Detail.tsx
+++ apps/financial-web/src/pages/Detail/Detail.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Center,
   CircularProgress,
   Flex,
   Grid,
@@ -31,76 +32,80 @@ const DetailPage = () => {
       isClosable: true,
     })
 
-  return isPending ? (
-    <CircularProgress isIndeterminate color="primary.100" />
-  ) : (
-    <Box maxW="1200px">
-      <Heading as="h1" textAlign="left">
-        {stockDetail.companyName} ({stockDetail.symbol})
-      </Heading>
-      <Text color="text.faded" fontSize="sm" mb="1rem">
-        {stockDetail.exchangeShortName}: {stockDetail.symbol} - Real-Time Price -{' '}
-        {stockDetail.currency}
-      </Text>
-      <Text fontSize="2.25rem" fontWeight="bold">
-        {stockDetail.price}
-        <Text
-          as="span"
-          fontSize="1.5rem"
-          fontWeight="semibold"
-          color={stockDetail.changes > 0 ? 'green.50' : 'red.50'}
-        >{` ${stockDetail.changes > 0 ? '+' : ''}${stockDetail.changes} (${((stockDetail.changes / stockDetail.price) * 100).toFixed(2)}%)`}</Text>
-      </Text>
+  return (
+    <Center>
+      {isPending ? (
+        <CircularProgress isIndeterminate color="primary.100" />
+      ) : (
+        <Box maxW="1200px">
+          <Heading as="h1" textAlign="left">
+            {stockDetail.companyName} ({stockDetail.symbol})
+          </Heading>
+          <Text color="text.faded" fontSize="sm" mb="1rem">
+            {stockDetail.exchangeShortName}: {stockDetail.symbol} - Real-Time Price -{' '}
+            {stockDetail.currency}
+          </Text>
+          <Text fontSize="2.25rem" fontWeight="bold">
+            {stockDetail.price.toFixed(2)}
+            <Text
+              as="span"
+              fontSize="1.5rem"
+              fontWeight="semibold"
+              color={stockDetail.changes > 0 ? 'green.50' : 'red.50'}
+            >{` ${stockDetail.changes > 0 ? '+' : ''}${stockDetail.changes.toFixed(2)} (${((stockDetail.changes / stockDetail.price) * 100).toFixed(2)}%)`}</Text>
+          </Text>
 
-      <StockPrice isIncrease={stockDetail.changes > 0} />
+          <StockPrice isIncrease={stockDetail.changes > 0} />
 
-      <Heading as="h2" textAlign="start" mb="0.5rem">
-        About {stockDetail.symbol.toUpperCase()}
-      </Heading>
-      <Text>{stockDetail.description}</Text>
-      <Grid templateColumns="repeat(2, 1fr)" gap={4}>
-        <GridItem gap="1rem">
-          <Flex direction="column" gap="0.5rem">
-            <Box>
-              <Text fontWeight="semibold">Industry</Text>
-              <Text>{stockDetail.industry}</Text>
-            </Box>
-            <Box>
-              <Text fontWeight="semibold">IPO Date</Text>
-              <Text>{dayjs(stockDetail.ipoDate).format('MMM DD, YYYY')}</Text>
-            </Box>
-            <Box>
-              <Text fontWeight="semibold">Stock Exchange</Text>
-              <Text>{stockDetail.exchangeShortName}</Text>
-            </Box>
-            <Box>
-              <Text fontWeight="semibold">Website</Text>
-              <Text color="text.link">
-                <Link href={stockDetail.website} textAlign="left" isExternal>
-                  {stockDetail.website}
-                </Link>
-              </Text>
-            </Box>
-          </Flex>
-        </GridItem>
-        <GridItem>
-          <Flex direction="column" gap="0.5rem">
-            <Box>
-              <Text fontWeight="semibold">Sector</Text>
-              <Text>{stockDetail.sector}</Text>
-            </Box>
-            <Box>
-              <Text fontWeight="semibold">Employees</Text>
-              <Text>{Intl.NumberFormat().format(+stockDetail.fullTimeEmployees)}</Text>
-            </Box>
-            <Box>
-              <Text fontWeight="semibold">Ticker Symbol</Text>
-              <Text>{stockDetail.symbol.toUpperCase()}</Text>
-            </Box>
-          </Flex>
-        </GridItem>
-      </Grid>
-    </Box>
+          <Heading as="h2" textAlign="start" mb="0.5rem">
+            About {stockDetail.symbol.toUpperCase()}
+          </Heading>
+          <Text>{stockDetail.description}</Text>
+          <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+            <GridItem gap="1rem">
+              <Flex direction="column" gap="0.5rem">
+                <Box>
+                  <Text fontWeight="semibold">Industry</Text>
+                  <Text>{stockDetail.industry}</Text>
+                </Box>
+                <Box>
+                  <Text fontWeight="semibold">IPO Date</Text>
+                  <Text>{dayjs(stockDetail.ipoDate).format('MMM DD, YYYY')}</Text>
+                </Box>
+                <Box>
+                  <Text fontWeight="semibold">Stock Exchange</Text>
+                  <Text>{stockDetail.exchangeShortName}</Text>
+                </Box>
+                <Box>
+                  <Text fontWeight="semibold">Website</Text>
+                  <Text color="text.link">
+                    <Link href={stockDetail.website} textAlign="left" isExternal>
+                      {stockDetail.website}
+                    </Link>
+                  </Text>
+                </Box>
+              </Flex>
+            </GridItem>
+            <GridItem>
+              <Flex direction="column" gap="0.5rem">
+                <Box>
+                  <Text fontWeight="semibold">Sector</Text>
+                  <Text>{stockDetail.sector}</Text>
+                </Box>
+                <Box>
+                  <Text fontWeight="semibold">Employees</Text>
+                  <Text>{Intl.NumberFormat().format(+stockDetail.fullTimeEmployees)}</Text>
+                </Box>
+                <Box>
+                  <Text fontWeight="semibold">Ticker Symbol</Text>
+                  <Text>{stockDetail.symbol.toUpperCase()}</Text>
+                </Box>
+              </Flex>
+            </GridItem>
+          </Grid>
+        </Box>
+      )}
+    </Center>
   )
 }
 
