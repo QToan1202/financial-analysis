a6e19ec103363745b4f58ae8ead2fe8b51f7ef31
Toan <92291488+QToan02@users.noreply.github.com>
2024-11-27 16:20:36 +0700

    Update nav link on Header

--- apps/financial-web/src/components/Header/Header.tsx
+++ apps/financial-web/src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import { logo } from '@assets'
 
 import { Search } from '../Search'
 import { Button } from '../Button'
+import { Link } from '../Link'
 
 const Header = () => (
   <chakra.header padding="0.5rem" boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px;" bgColor="white">
@@ -12,12 +13,16 @@ const Header = () => (
         <Search />
       </Flex>
       <Flex gap="0.25rem">
-        <Button variant="ghost" size="lg" borderRadius="2px">
-          Log In
-        </Button>
-        <Button size="lg" borderRadius="2px">
-          Sign Up
-        </Button>
+        <Link to="log-in">
+          <Button variant="ghost" size="lg" borderRadius="2px">
+            Log In
+          </Button>
+        </Link>
+        <Link to="sign-up">
+          <Button size="lg" borderRadius="2px">
+            Sign up
+          </Button>
+        </Link>
       </Flex>
     </Flex>
   </chakra.header>
