4e3d2b424dc5afb9de7b8aebfea7c09967108eb2
Toan <92291488+QToan02@users.noreply.github.com>
2024-12-25 10:27:19 +0700

    Update runtime model in Express BE

--- apps/financial-api/src/controller/copilotkit.ts
+++ apps/financial-api/src/controller/copilotkit.ts
@@ -6,17 +6,14 @@ import {
 } from '@copilotkit/runtime'
 import OpenAI from 'openai'
 
-const openai = new OpenAI({
-  apiKey: process.env.NVIDIA_API_KEY,
-  baseURL: 'https://integrate.api.nvidia.com/v1',
-})
-const serviceAdapter = new OpenAIAdapter({ openai, model: 'meta/llama-3.1-405b-instruct' })
+const openai = new OpenAI()
+const serviceAdapter = new OpenAIAdapter({ openai, model: 'gpt-4o-mini' })
 
 export const runtime = (request: any, response: any, next: NextFunction) => {
   const runtime = new CopilotRuntime({
     remoteActions: [
       {
-        url: 'http://localhost:8000/copilotkit_remote',
+        url: `${process.env.FASTAPI_BASE_URL}/copilotkit_remote`,
       },
     ],
   })
